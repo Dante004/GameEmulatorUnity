b4f82599ed3d5e0969dbe44cc3b7069cbc2d7220
szymon <szymon.brzozowski23@wp.pl>
2019-03-14 14:46:54 +0100

    added loading image function

--- GBEUnity/Assets/Menu/Scripts/DetectingFiles.cs
+++ GBEUnity/Assets/Menu/Scripts/DetectingFiles.cs
@@ -4,31 +4,37 @@ using UnityEngine;
 using System.IO;
 using UnityEngine.UI;
 using System;
+using System.Linq;
 public class DetectingFiles : MonoBehaviour
 {
     public GameObject textPrefab;
     public GameObject titlePrefab;
     public int numberOfFiles = 0;
+    [NonSerialized]
     public string[] files;
+    public string[] posters;
     public int positionY = 10;
     public int positionX = 100;
+    public int numberOfPosters = 0;
+  public Sprite sprite;
+    public string TMP;
     // Start is called before the first frame update
-    
+
     void Start()
     {
         if (Directory.Exists(Path.GetFullPath("Roms")))
         {
-           
+            Dictionary<string, Sprite> obrazki = Resources.LoadAll<Sprite>("img").ToDictionary(x => x.name.ToUpper(), x => x);
             files = Directory.GetFileSystemEntries(Path.GetFullPath("Roms"));
             for (int i = 0; i < files.Length; i++)
             {
-                if (!files[i].Contains(".meta") && files[i].Contains(".gb"))
+                if (!files[i].EndsWith(".meta") && files[i].EndsWith(".gb"))
                 {
                     numberOfFiles++;
-                    FileInfo File = new FileInfo(files[i]);
+                    FileInfo Files = new FileInfo(files[i]);
                     GameObject prefabTextTmp = Instantiate(textPrefab) as GameObject;
                     Text textName = prefabTextTmp.GetComponentInChildren<Text>();
-                    textName.text = "Files :" + File.Name;
+                    textName.text = "Files :" + Files.Name;
                     prefabTextTmp.transform.SetParent(gameObject.transform, false);
                     RectTransform rect = prefabTextTmp.GetComponent<RectTransform>();
                     rect.localPosition = new Vector2(prefabTextTmp.transform.position.x, positionY);
@@ -36,11 +42,11 @@ public class DetectingFiles : MonoBehaviour
                     textName.horizontalOverflow = HorizontalWrapMode.Overflow;
                     textName.verticalOverflow = VerticalWrapMode.Overflow;
                     textName.alignment = TextAnchor.MiddleCenter;
-                    RomGame romGame = ROMLoader.Load(files[i]);
-                    //Debug.Log(romGame.title);
+                    RomGame romGame = ROMLoader.Load(files[i]);                   
                     GameObject prefabTitleTmp = Instantiate(titlePrefab) as GameObject;
                     Text title = prefabTitleTmp.GetComponentInChildren<Text>();
                     title.text = romGame.title.ToString();
+                    TMP = romGame.title.ToString();
                     prefabTitleTmp.transform.SetParent(gameObject.transform, false);
                     RectTransform rectTitle = prefabTitleTmp.GetComponent<RectTransform>();
                     rectTitle.localPosition = new Vector2(positionX,100 );
@@ -48,12 +54,12 @@ public class DetectingFiles : MonoBehaviour
                     title.horizontalOverflow = HorizontalWrapMode.Overflow;
                     title.verticalOverflow = VerticalWrapMode.Overflow;
                     title.alignment = TextAnchor.MiddleCenter;
-                     
+                    posters = Directory.GetFileSystemEntries("Assets\\Resources");
+                    prefabTitleTmp.GetComponentInChildren<Image>().sprite = obrazki[TMP];
                 }
                 positionY -= 30;
-                positionX -= 200;
-            }
-        }
+                positionX -= 200;            
+            }           
+        }        
     }
-
 }
