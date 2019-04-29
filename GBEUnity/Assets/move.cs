d9ef8de977e6931fc42435e00a4a39eb6fa7d19d
szymon <szymon.brzozowski23@wp.pl>
2019-04-29 17:15:47 +0200

    add moving images and new roms

---
+++ GBEUnity/Assets/move.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * (Time.deltaTime+1.5f));
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * (Time.deltaTime+1.5f));
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("wybrano element");
+        }
+    }
+   public void click()
+    {
+        Debug.Log("wybrano element");
+    }
+}
---
+++ GBEUnity/Assets/movingPrefab.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movingPrefab : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+      //  float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, 0.0f );
+        rb.AddForce(movement*speed);
+    }
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+}
