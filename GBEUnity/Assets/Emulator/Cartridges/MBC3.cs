7b1d5c0c0c52285a7014be269f62c0fe3af4dd8b
Dante <bartosz.bk04@gmail.com>
2019-08-15 14:29:51 +0200

    Checking if rtc and ram is enable in mbc3

--- GBEUnity/Assets/Emulator/Cartridges/MBC3.cs
+++ GBEUnity/Assets/Emulator/Cartridges/MBC3.cs
@@ -9,9 +9,11 @@ namespace Emulator.Cartridges
         private const int RamBankSize = 8192;
         private int _selectedRomBank = 1;
         private int _selectedRamBank = 0;
+        private int _rtcRegister = 0;
         private readonly byte[,] _ram = new byte[RamBank, RamBankSize];
         private readonly byte[,] _rom;
-        private bool _ramRTCEnambleFlag = false;
+        private bool _rtcEnable = false;
+        private bool _ramEnable = false;
         private DateTime _latchClock;
         private int _latchClockData = 0x01;
 
@@ -43,29 +45,41 @@ namespace Emulator.Cartridges
             }
             else if (address >= 0xA000 && address <= 0xBFFF)
             {
-                if (_ramRTCEnambleFlag)
+                if (_selectedRamBank >= 0)
                 {
-                    if (_selectedRamBank < 4)
+                    if (_ramEnable)
                     {
                         return _ram[_selectedRamBank, address - 0xA000];
                     }
                     else
                     {
-                        Debug.Log("Get RTC register");
-                        switch (_selectedRamBank)
-                        {
-                            case 0x08:
-                                return _latchClock.Second;
-                            case 0x09:
-                                return _latchClock.Minute;
-                            case 0x0A:
-                                return _latchClock.Hour;
-                            case 0x0B:
-                                return _latchClock.DayOfYear & 0x00FF;
-                            case 0x0C:
-                                return (_latchClock.DayOfYear & 0x01FF) >> 8;
-                        }
+                        Debug.LogError($"Attempting to read from disabled ram {address:X}");
+                        return 0xFF;
                     }
+                }
+                else if (_rtcEnable)
+                {
+                    Debug.LogError("Get RTC register");
+                    switch (_rtcRegister)
+                    {
+                        case 0x08:
+                            return _latchClock.Second;
+                        case 0x09:
+                            return _latchClock.Minute;
+                        case 0x0A:
+                            return _latchClock.Hour;
+                        case 0x0B:
+                            return _latchClock.DayOfYear & 0x00FF;
+                        case 0x0C:
+                            return (_latchClock.DayOfYear & 0x01FF) >> 8;
+                        default:
+                            return 0xFF;
+                    }
+                }
+                else
+                {
+                    Debug.LogError($"Attempting to read from disabled rtc {address:X}");
+                    return 0xFF;
                 }
             }
             Debug.LogError($"Invalid cartridge read: {address:X}");
@@ -76,11 +90,12 @@ namespace Emulator.Cartridges
         {
             if (address <= 0x1FFF)
             {
-                _ramRTCEnambleFlag = (value & 0x0A) == 0x0A;
+                _ramEnable = (value & 0x0F) == 0x0A;
+                _rtcEnable = (value & 0x0F) == 0x0A;
             }
             else if (address >= 0x2000 && address <= 0x3FFF)
             {
-                _selectedRomBank = 0x7F & value;
+                _selectedRomBank = value & 0x7F;
                 if (_selectedRomBank == 0x00)
                 {
                     _selectedRomBank = 0x01;
@@ -88,7 +103,26 @@ namespace Emulator.Cartridges
             }
             else if (address >= 0x4000 && address <= 0x5FFF)
             {
-                _selectedRamBank = 0x0F & value;
+                if (value >= 0x08 && value <= 0x0C)
+                {
+                    if (_rtcEnable)
+                    {
+                        _rtcRegister = value;
+                        _selectedRamBank = -1;
+                    }
+                    else
+                    {
+                        Debug.LogError($"Attempting to write rtc register when rtc is disabled {address:X}, {value:X}");
+                    }
+                }
+                else if (value <= 0x03)
+                {
+                    _selectedRamBank = value;
+                }
+                else
+                {
+                    Debug.LogError($"Attempting to select unknown register {address:X}, {value:X}");
+                }
             }
             else if (address >= 0x6000 && address <= 0x7FFF)
             {
@@ -98,9 +132,41 @@ namespace Emulator.Cartridges
             }
             else if (address >= 0xA000 && address <= 0xBFFF)
             {
-                if (_ramRTCEnambleFlag && _selectedRamBank < 4)
+                if (_selectedRamBank >= 0)
                 {
-                    _ram[_selectedRamBank, address - 0xA000] = (byte)(0xFF & value);
+                    if (_ramEnable)
+                    {
+                        _ram[_selectedRamBank,address - 0xA000] = (byte) (value & 0xFF);
+                    }
+                    else
+                    {
+                        Debug.LogError($"Attempting to write on ram when ram is disabled {address:X}, {value:X}");
+                    }
+                }
+                else if(_rtcEnable)
+                {
+                    switch (_rtcRegister)
+                    {
+                        case 0x08:
+                            _latchClock.AddSeconds(value);
+                            break;
+                        case 0x09:
+                            _latchClock.AddMinutes(value);
+                            break;
+                        case 0x0A:
+                            _latchClock.AddHours(value);
+                            break;
+                        case 0x0B:
+                            _latchClock.AddDays(value);
+                            break;
+                        case 0x0C:
+                            _latchClock.AddDays(_latchClock.DayOfYear & 0x80 | value & 0xC1);
+                            break;
+                    }
+                }
+                else
+                {
+                    Debug.LogError($"Attempting to write on rtc when rtc is disabled {address:X}, {value:X}");
                 }
             }
             else
