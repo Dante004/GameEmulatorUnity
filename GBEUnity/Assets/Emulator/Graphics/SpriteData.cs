f21ec58f70a949c0e0156505446e7391ae2625bc
Dante <bartosz.bk04@gmail.com>
2019-09-29 11:56:11 +0200

    Adding data to ppu

---
+++ GBEUnity/Assets/Emulator/Graphics/SpriteData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Emulator.Graphics
+{
+    /// <summary>
+    /// Represents the structure of a sprite located in the Object Attribute Memory (OAM).
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct SpriteData
+    {
+        public byte Y;
+        public byte X;
+        public byte TileDataIndex;
+        public SpriteDataFlags Flags;
+    }
+
+    /// <summary>
+    /// Provides members for representing all the valid sprite atrributes.
+    /// </summary>
+    [Flags]
+    public enum SpriteDataFlags : byte
+    {
+        None = 0,
+        PaletteNumberMask = 0b111,
+        TileVramBank = (1 << 3),
+        UsePalette1 = (1 << 4),
+        XFlip = (1 << 5),
+        YFlip = (1 << 6),
+        BelowBackground = (1 << 7)
+    }
+}
